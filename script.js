e3536f6a465f521dba588da4ac338ef0c35713d0
Koushlendra Kuamr <code.koushlendra@gmail.com>
2022-02-19 12:12:54 +0530

    Change input methods of color and grid size

--- script.js
+++ script.js
@@ -1,90 +1,46 @@
-let gridSize =3;
-let bgColor = 'black';
+
+const colorInp = document.querySelector('#color');
+const sizeInp = document.querySelector('#grid-size');
 const container = document.querySelector('.container');
-const blackColor = document.querySelector('.black-color');
-blackColor.addEventListener('click',()=>{
-    blackColor.classList.add('active');
-    bgColor = 'black';
-    randomColor.classList.remove('active');
+const randomColor = document.querySelector('.random-color');
+const eraser = document.querySelector('.erase');
+const deleteGrid = document.querySelector('.clear-grid');
+const gridRangeOutput = document.querySelector('.grid-range');
+let gridSize =sizeInp.value;
+let bgColor = 'userSelection';
+
+gridRangeOutput.textContent = sizeInp.value + ' X '+ sizeInp.value;
+sizeInp.addEventListener('input', ()=>{
+    gridRangeOutput.textContent = sizeInp.value + ' X '+ sizeInp.value;
+    gridSize = sizeInp.value;
+    clearGrid();
+    createGrid(gridSize);
+});
+colorInp.addEventListener('input',()=>{
+    bgColor = 'userSelection';
     eraser.classList.remove('active');
+    randomColor.classList.remove('active');
 });
 
-const randomColor = document.querySelector('.random-color');
 randomColor.addEventListener('click', ()=>{
     randomColor.classList.add('active');
     bgColor='random';
-    blackColor.classList.remove('active');
     eraser.classList.remove('active');
 });
 
-const eraser = document.querySelector('.erase');
+
 eraser.addEventListener('click', ()=>{
     eraser.classList.add('active');
     bgColor = 'white';
-    blackColor.classList.remove('active');
     randomColor.classList.remove('active');
 });
 
-const deleteGrid = document.querySelector('.clear-grid');
 deleteGrid.addEventListener('click', ()=>{
     clearGrid();
-    createGrid(3);
-    customize.classList.remove('active');
-    smallGrid.classList.remove('active');
-    mediumGrid.classList.remove('active');
-    bigGrid.classList.remove('active');
+    sizeInp.value = 4;
+    gridRangeOutput.textContent = sizeInp.value + ' X '+ sizeInp.value;
+    createGrid(4);
 });
-
-
-const smallGrid = document.querySelector('.small');
-smallGrid.addEventListener('click', ()=> {
-    clearGrid();
-    createGrid(16);
-    smallGrid.classList.add('active');
-    mediumGrid.classList.remove('active');
-    bigGrid.classList.remove('active');
-    customize.classList.remove('active');
-});
-        
-
-const mediumGrid = document.querySelector('.medium');
-mediumGrid.addEventListener('click', ()=>{
-    clearGrid();
-    createGrid(32);
-    mediumGrid.classList.add('active');
-    smallGrid.classList.remove('active');
-    bigGrid.classList.remove('active');
-    customize.classList.remove('active');
- });
-
-const bigGrid = document.querySelector('.big');
-bigGrid.addEventListener('click', () =>{
-    clearGrid();
-    createGrid(64);
-    bigGrid.classList.add('active');
-    smallGrid.classList.remove('active');
-    mediumGrid.classList.remove('active');
-    customize.classList.remove('active');
-});
-
-const customize = document.querySelector('.customize');
-customize.addEventListener('click', ()=>{
-    customize.classList.add('active');
-    let userInput = prompt('Enter grid size (maximum: 100)');
-    if(userInput<=100){
-        clearGrid();
-        createGrid(userInput);
-        smallGrid.classList.remove('active');
-        mediumGrid.classList.remove('active');
-        bigGrid.classList.remove('active');
-    }else if(userInput>100){
-        customize.classList.remove('active');
-        alert('Value not allowed');
-    }else if(typeof userInput !== 'number'){
-        customize.classList.remove('active');
-        alert('Invalid Input! Try Again.');
-    }
-}); 
 
 function createGrid(gridSize){
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
@@ -92,7 +48,7 @@ function createGrid(gridSize){
     for(let i = 1; i<=gridSize*gridSize; i++){
         const grid = document.createElement('div');
         grid.classList.add('grid');
-        grid.addEventListener('mousemove', changeColor);
+        grid.addEventListener('mouseenter', changeColor);
         container.appendChild(grid);
        }
 }
@@ -107,8 +63,8 @@ function changeColor(e){
         e.target.style.backgroundColor = 'white';
     }else if(bgColor =='random'){
         e.target.style.backgroundColor =`hsl(${Math.random()* 360},100% ,50%)`;
-    }else if(bgColor=='black'){
-        e.target.style.backgroundColor = 'black';
+    }else if(bgColor=='userSelection'){
+        e.target.style.backgroundColor = colorInp.value;
     }
    
 }
